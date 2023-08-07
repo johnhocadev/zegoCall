fa56ab53864a31c6d313a7ada8008c3b8cec3eee
Islam-hoca <islam@hocadev.onmicrosoft.com>
2023-08-07 13:42:54 +0500

    Main Functions are added

---
+++ lib/login_service.dart
@@ -0,0 +1,70 @@
+// Package imports:
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
+
+// Project imports:
+import 'common.dart';
+import 'constants.dart';
+
+ZegoUIKitPrebuiltCallController? callController;
+
+/// local virtual login
+Future<void> login({
+  required String userID,
+  required String userName,
+}) async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.setString(cacheUserIDKey, userID);
+
+  currentUser.id = userID;
+  currentUser.name = 'user_$userID';
+}
+
+/// local virtual logout
+Future<void> logout() async {
+  final prefs = await SharedPreferences.getInstance();
+  prefs.remove(cacheUserIDKey);
+}
+
+/// on user login
+void onUserLogin() {
+  callController ??= ZegoUIKitPrebuiltCallController();
+
+  /// 4/5. initialized ZegoUIKitPrebuiltCallInvitationService when account is logged in or re-logged in
+  ZegoUIKitPrebuiltCallInvitationService().init(
+    appID: 1192459651 /*input your AppID*/,
+    appSign: "fb0faed6aadb25501f092c711ab20c48cb7b26ece9fc17ab7e64c31e20245f61" /*input your AppSign*/,
+    userID: currentUser.id,
+    userName: currentUser.name,
+    notifyWhenAppRunningInBackgroundOrQuit: false,
+    plugins: [ZegoUIKitSignalingPlugin()],
+    controller: callController,
+    requireConfig: (ZegoCallInvitationData data) {
+      final config = (data.invitees.length > 1)
+          ? ZegoCallType.videoCall == data.type
+          ? ZegoUIKitPrebuiltCallConfig.groupVideoCall()
+          : ZegoUIKitPrebuiltCallConfig.groupVoiceCall()
+          : ZegoCallType.videoCall == data.type
+          ? ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall()
+          : ZegoUIKitPrebuiltCallConfig.oneOnOneVoiceCall();
+
+      config.avatarBuilder = customAvatarBuilder;
+
+      /// support minimizing, show minimizing button
+      config.topMenuBarConfig.isVisible = true;
+      config.topMenuBarConfig.buttons
+          .insert(0, ZegoMenuBarButtonName.minimizingButton);
+
+      return config;
+    },
+  );
+}
+
+/// on user logout
+void onUserLogout() {
+  callController = null;
+
+  /// 5/5. de-initialization ZegoUIKitPrebuiltCallInvitationService when account is logged out
+  ZegoUIKitPrebuiltCallInvitationService().uninit();
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,24 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
+import awesome_notifications
+import device_info_plus
+import path_provider_foundation
+import shared_preferences_foundation
+import sqflite
+import wakelock_macos
+import zego_express_engine
+import zego_zim
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
+  ZegoExpressEnginePlugin.register(with: registry.registrar(forPlugin: "ZegoExpressEnginePlugin"))
+  ZegoZimPlugin.register(with: registry.registrar(forPlugin: "ZegoZimPlugin"))
 }
